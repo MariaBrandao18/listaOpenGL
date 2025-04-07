125e2c17cfc6ecad8d9ad04bf53df3d08165c8c9
chico2302 <chicobr1234@gmail.com>
2025-04-07 08:31:05 -0300

    Create ex4.cpp

---
+++ ex4.cpp
@@ -0,0 +1,41 @@
+#include <GL/glut.h>
+
+float x_position = 0.0f;
+float velocidade = 0.00001f;
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.5, 0, 1);
+
+	glBegin(GL_QUADS);
+	glVertex2f(10 + x_position, 10);
+	glVertex2f(10 + x_position, -10);
+	glVertex2f(-10 + x_position, -10);
+	glVertex2f(-10 + x_position, 10);
+	glEnd();
+	glFlush();
+}
+
+void animar(int timer) {
+	glutPostRedisplay();
+	x_position -= velocidade;
+	if (x_position > 90 || x_position < -90) {
+		velocidade = -velocidade;
+	}
+	glutTimerFunc(timer, animar, 0);
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowSize(640, 640);
+	glutCreateWindow("Transformacoes");
+	glutDisplayFunc(display);
+	glutTimerFunc(1000, animar, 0);
+	gluOrtho2D(-100, 100, -100, 100);
+	glClearColor(1, 1, 1, 0);
+	glutMainLoop();
+	return 0;
+}
